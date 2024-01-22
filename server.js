8c95678eb76abaf9f40dc98ccaa5c7051d6350df
Daniels <job.business.just@gmail.com>
2024-01-22 04:21:33 +0300

    update

--- server.js
+++ server.js
@@ -213,13 +213,6 @@ app.post('/submit', async (req, res) => {
 
     await browser.close();
     
-    await Promise.all(globalData.uploadedFiles.map(async (image) => {
-      const imagePath = path.join(__dirname, `/uploads/images/${userId}/${userId}_${image.originalname}`);
-      await fs.promises.unlink(imagePath);
-    }));
-
-    const userFolderPath = path.join(__dirname, `/uploads/images/${userId}`);
-    await fsExtra.remove(userFolderPath);
     globalData.uploadedFiles = [];
 
     res.setHeader('Content-Type', 'application/pdf');

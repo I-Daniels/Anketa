7f2ec79513efc0767a1f863c2471653bdde26624
Daniels <job.business.just@gmail.com>
2024-02-26 14:15:18 +0300

    update

--- public/js/script.js
+++ public/js/script.js
@@ -313,7 +313,6 @@ function checkFields(indices) {
 var fieldsToCheck = [1, 2, 3, 4, 5, 16, 18, 19, 20, 23, 30, 31, 32];
 async function sendButtonClicked() {
   try {
-    // 
     if (!checkNameFields() || !checkNameFields() || !checkFields(fieldsToCheck) || !checkPassportFields() || !checkIfPhotosUploaded()) {
       return;
     }
